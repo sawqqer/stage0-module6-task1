0c0563ef49a9074d2f70ca92f5ba7e9f9c9c4ed1
Arman <gcom0733@gmail.com>
2023-03-15 20:50:40 +0600

    fixed errors

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,43 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public void setNumberOfPaws(int numberOfPaws) {
+        this.numberOfPaws = numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public void setHasFur(boolean hasFur) {
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String a = hasFur ? "a" : "no";
+        String plural = numberOfPaws == 1 ? "" : "s";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " paw" + plural + " and " + a + " fur.";
+    }
 }
